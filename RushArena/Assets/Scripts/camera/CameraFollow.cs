6f6e69b04a7a2054737e50e29799204058d4fba7
Ro-Roy <83682204+Ro-Roy@users.noreply.github.com>
2022-03-10 02:44:18 +0100

    prototype caméra follow
    encore un commit du crack tu connais:
    - le camera follow est disponible dans
    la scène movement pr l'instant
    -ptit fix d'un bug qui faisait perdre l'équilibre au joueur quand il rencontrait un obstacle

---
+++ RushArena/Assets/Scripts/camera/CameraFollow.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector2 velocity;
+
+    public float smoothTimeX;
+    public float smoothTimeY;
+
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
