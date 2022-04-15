155401dd54d4bfb0b8088b6d299ae5cecbb1def3
Ro-Roy <83682204+Ro-Roy@users.noreply.github.com>
2022-04-15 19:08:10 +0200

    collision between props and player disabled

--- RushArena/Assets/Scripts/PlayerCollisionScript.cs
+++ RushArena/Assets/Scripts/PlayerCollisionScript.cs
@@ -11,7 +11,7 @@ public class PlayerCollisionScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Physics.IgnoreLayerCollision(3,7);
     }
 
     // Update is called once per frame
