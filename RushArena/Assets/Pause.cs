9b6c5a5331fb1d112234b925d7bba9d15cb173d0
rayan943 <rayanchelabi@gmail.com>
2022-04-16 16:38:48 +0200

    Pause Menu
    - add pause menu
    - add button asset
    - add timer interface

---
+++ RushArena/Assets/Pause.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+
+    [SerializeField] private GameObject pauseMenu;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        
+        Debug.Log("Quit");
+    }
+}
